27fd82b881edfef9096dcfbc3051f512497b93d8
Bharadwaj <admin@bajjus.com>
2018-07-08 21:03:39 +1000

    ReactForm - initial Commit

---
+++ src/containers/home/Container.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import Component from './Component';
+
+
+
+export default connect()(Component);
---
+++ src/containers/home/index.js
@@ -0,0 +1,2 @@
+export { default as Home } from './Container';
+export { default as HomeReducer } from './reducer';
